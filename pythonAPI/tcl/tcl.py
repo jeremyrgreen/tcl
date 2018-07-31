562258c64642f2087968df61016cce2ba95d9127
Johnnie Gray <johnniemcgray@gmail.com>
2018-07-31 13:15:32 +0100

    add einsum, tensordot

--- pythonAPI/tcl/tcl.py
+++ pythonAPI/tcl/tcl.py
@@ -15,10 +15,10 @@ except:
 lib = cdll.LoadLibrary(TCL_ROOT+"/lib/libtcl.so")
 
 def randomNumaAwareInit( A ):
-    """ 
+    """
     initializes the passed numpy.ndarray (which have to be created with
     numpy.empty) and initializes it with random data in paralle such that the
-    pages are equally distributed among the numa nodes 
+    pages are equally distributed among the numa nodes
     """
     lib.randomNumaAwareInit( ctypes.c_void_p(A.ctypes.data),
             ctypes.cast(A.ctypes.shape, ctypes.POINTER(ctypes.c_voidp)),
@@ -26,9 +26,9 @@ def randomNumaAwareInit( A ):
 
 
 def tensorMult( alpha, A, indicesA, B, indicesB, beta,  C, indicesC):
-    """ 
+    """
         This function computes the tensor contraction of A and B, yielding C.
-        The tensor contraction is of the form: 
+        The tensor contraction is of the form:
            C[indicesC] = alpha * A[indicesA] * B[indicesB] + beta * C[indicesC]
 
         where alpha and beta are scalors and A, B, and C correspond to arbitrary
@@ -66,7 +66,7 @@ def tensorMult( alpha, A, indicesA, B, indicesB, beta,  C, indicesC):
                         ctypes.c_double(beta) , dataC, sizeC, outerSizeC, indicesC, useRowMajor)
 
 def equal(A, B, numSamples=-1):
-    """ Ensures that alle elements of A and B are pretty much equal (due to limited machine precision) 
+    """ Ensures that alle elements of A and B are pretty much equal (due to limited machine precision)
 
     Parameter:
     numSamples: number of random samples to compare (-1: all). This values is used to approximate this function and speed the result up."
@@ -91,3 +91,100 @@ def equal(A, B, numSamples=-1):
          if(relError > 4e-5 and min(Aabs,Babs) > threshold ):
             error += 1
     return error == 0
+
+
+def einsum(eq, A, B, out=None):
+    """Two term tensor contraction using einsum-like api.
+
+    Parameters
+    ----------
+    eq : str
+        The einsum equation.
+    A, B : array_like, len(shape) >= 1
+        Tensors to "dot".
+    """
+    # parse the input equation into indices
+    if "->" in eq:
+        lhs, C_ix = eq.split('->')
+        A_ix, B_ix = lhs.split(',')
+    else:
+        A_ix, B_ix = eq.split(',')
+        idx_rm = [i for i in A_ix if i in B_ix]
+        C_ix = "".join(i for i in sorted(A_ix + B_ix) if i not in idx_rm)
+
+    # figure out the output dtype, upcast inputs if they differ in dtype
+    if A.dtype == B.dtype:
+        dtype = A.dtype
+    else:
+        dtype = np.common_type(A, B)
+        A, B = A.astype(dtype), B.astype(dtype)
+
+    # create out if it is not supplied
+    if out is None:
+        sz_dict = {i: (A.shape[A_ix.find(i)] if i in A_ix else
+                       B.shape[B_ix.find(i)]) for i in A_ix + B_ix}
+        out = np.empty([sz_dict[i] for i in C_ix], dtype=dtype)
+
+    # add commas between indices for tcl format
+    indA, indB, indC = (",".join(x) for x in (A_ix, B_ix, C_ix))
+
+    # perform the contraction!
+    tensorMult(1.0, A, indA, B, indB, 0.0, out, indC)
+    return out
+
+
+einsum_symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+def tensordot(A, B, axes=2):
+    """Simple translation of tensordot syntax to index notation.
+
+    Parameters
+    ----------
+    A, B : array_like, len(shape) >= 1
+        Tensors to "dot".
+    axes : int or (2,) array_like
+        * integer_like
+          If an int N, sum over the last N axes of `a` and the first N axes
+          of `b` in order. The sizes of the corresponding axes must match.
+        * (2,) array_like
+          Or, a list of axes to be summed over, first sequence applying to `a`,
+          second to `b`. Both elements array_like must be of the same length.
+    """
+    # convert int argument to (list[int], list[int])
+    if isinstance(axes, int):
+        axes = range(A.ndim - axes, A.ndim), range(axes)
+
+    # convert (int, int) to (list[int], list[int])
+    if isinstance(axes[0], int):
+        axes = (axes[0],), axes[1]
+    if isinstance(axes[1], int):
+        axes = axes[0], (axes[1],)
+
+    # initialize empty indices
+    A_ix = [None] * A.ndim
+    B_ix = [None] * B.ndim
+    C_ix = []
+
+    # fill in repeated indices
+    available_ix = iter(einsum_symbols)
+    for ax1, ax2 in zip(*axes):
+        repeat = next(available_ix)
+        A_ix[ax1] = repeat
+        B_ix[ax2] = repeat
+
+    # fill in the rest, and maintain output order
+    for i in range(A.ndim):
+        if A_ix[i] is None:
+            leave = next(available_ix)
+            A_ix[i] = leave
+            C_ix.append(leave)
+    for i in range(B.ndim):
+        if B_ix[i] is None:
+            leave = next(available_ix)
+            B_ix[i] = leave
+            C_ix.append(leave)
+
+    # form full string and contract!
+    einsum_str = "{},{}->{}".format(*map("".join, (A_ix, B_ix, C_ix)))
+    return einsum(einsum_str, A, B)
